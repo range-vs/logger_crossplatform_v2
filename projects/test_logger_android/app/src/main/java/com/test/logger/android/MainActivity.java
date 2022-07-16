ad3b549f3f691e17e13ca6a5c430d9e8fe1a41bb
range36rus <trubnicov_ivan1996@mail.ru>
2022-07-16 12:35:17 +0300

    + get check storage permission

--- projects/test_logger_android/app/src/main/java/com/test/logger/android/MainActivity.java
+++ projects/test_logger_android/app/src/main/java/com/test/logger/android/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = findViewById(R.id.sample_text);
         tv.setText("Test logger app");
 
-        initLogger();
+        checkForStoragePermission();
 
     }
 
@@ -62,6 +62,31 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    private static final int APPS_PERMISSIONS_EXTERNAL_STORAGE = 10000;
+
+    private void checkForStoragePermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != 0) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, APPS_PERMISSIONS_EXTERNAL_STORAGE);
+            return;
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case APPS_PERMISSIONS_EXTERNAL_STORAGE: {
+                if (permissions[0].equalsIgnoreCase(Manifest.permission.WRITE_EXTERNAL_STORAGE) && grantResults[0] == 0) {
+                    initLogger();
+                } else {
+                    String msg = String.format("You must enable %s permission to enable logger", Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                    Log.d(TAG, msg);
+                    Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     public native void initLogger(String pathLog, Activity activity);
